ba166ecec509477b6a73ad79cdf76b56b2374b1f
Divas <92243128+divasgt@users.noreply.github.com>
2022-10-03 00:53:11 +0530

    Added Linear Search in Java

---
+++ Coders/DSA Concepts/LinearSearch.java
@@ -0,0 +1,10 @@
+public class LinearSearch {
+    public static int linearSearch(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
