d4946d5a70d8a303c792a8d6cd86a7df8a4fcc80
Divas <92243128+divasgt@users.noreply.github.com>
2022-10-03 00:40:37 +0530

    Added Binary Search in Java

---
+++ Coders/DSA Concepts/BinarySearch.java
@@ -0,0 +1,19 @@
+public class BinarySearch {
+    public static int binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while (start<=end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid]<target) {
+                start=mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+}
