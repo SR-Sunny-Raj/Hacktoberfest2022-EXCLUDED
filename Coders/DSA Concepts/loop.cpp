5535495fda6afb52cdfbd7702e34cf235e9399e2
Sumit Kumar <81135212+nxsumityadav@users.noreply.github.com>
2022-10-03 21:26:33 +0530

    added loop while loop Beginners

---
+++ Coders/DSA Concepts/loop.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    //********** LEARNING WHILE LOOP ***************
+     
+    /*      int n;
+            cin>>n;
+            int i = 1;
+
+            while(i<=n)
+            {
+                cout<<i<<endl;
+                i = i+1;
+                }
+                
+                return 0;
+             } 
+        
+    */ 
+
+//********** SUM OF N NATURAL NUMBERS ***************
+
+    /*      int n;
+            cin>>n;
+
+            int i = 1;
+            int sum = 0;
+            while(i<=n){
+                sum = sum + i;
+                i = i+1;
+
+            }
+            cout<<sum<<endl; 
+    */
+
+
+ //********** FORMULA SHEET ***************
+
+    // cout<<n*(n+1)/2; --------sum of n natural numbers
+    // n(n+1)---------- sum of n even numbers
+    // C = (F-32)/1.8 -------  Converting Fahrenheit to celsius.
+
+
+
+
+
+    
+return 0;
+}
