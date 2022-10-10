1e6faba84b734aef12fdc29eaae3331a8943cf5c
codexvikram <officialvk001@gmail.com>
2022-10-10 14:41:32 +0530

    added binarytree.c linkedlistinsertion.c queue using linkedlist.c modified binarysearch code in cpp

--- Coders/DSA Concepts/Binary_Search.cpp
+++ Coders/DSA Concepts/Binary_Search.cpp
@@ -6,80 +6,25 @@ using namespace std;
 
  
 
-int binarySearch(vector<int> v, int To_Find)
-
-{
-
-    int lo = 0, hi = v.size() - 1;
-
-    int mid;
-
-    // This below check covers all cases , so need to check
-
-    // for mid=lo-(hi-lo)/2
-
-    while (hi - lo > 1) {
-
-        int mid = (hi + lo) / 2;
-
-        if (v[mid] < To_Find) {
-
-            lo = mid + 1;
-
-        }
-
-        else {
-
-            hi = mid;
-
-        }
-
+int binarySearch(int array[],int p,int l,int h){
+    while(l<=h){
+        int m=l+(h-l)/2;
+        if(array[m]==x)
+            return m;
+        if(arr[m]<x)
+            l=m+1;
+        else
+            h=m-1;
     }
-
-    if (v[lo] == To_Find) {
-
-        cout << "Found"
-
-             << " At Index " << lo << endl;
-
-    }
-
-    else if (v[hi] == To_Find) {
-
-        cout << "Found"
-
-             << " At Index " << hi << endl;
-
-    }
-
-    else {
-
-        cout << "Not Found" << endl;
-
-    }
-
+    return -1;
 }
-
- 
-
-int main()
-
-{
-
-    vector<int> v = { 1, 3, 4, 5, 6 };
-
-    int To_Find = 1;
-
-    binarySearch(v, To_Find);
-
-    To_Find = 6;
-
-    binarySearch(v, To_Find);
-
-    To_Find = 10;
-
-    binarySearch(v, To_Find);
-
-    return 0;
-
+int main(void){
+    int array[]={6,4,5,8,9,2,3};
+    int k=4;
+    int n= sizeof(array)/sizeof(array[0]);
+    int sol=binarySearch(array,k,0,n-1);
+    if(sol==-1)
+        printf("Not exist");
+    else
+        printf("Element is exists at index %d",sol);
 }
